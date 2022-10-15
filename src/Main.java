881617a8801699f38571ed9adcc0d847bd07462e
mehmetkino <109981439+mehmetkino@users.noreply.github.com>
2022-10-14 20:49:06 -0400

    Update Main.java
    just added a new println statement

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("I am writing this from my GitHub account);
+                           
     }
 }
